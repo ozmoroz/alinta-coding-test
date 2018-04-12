31464e38fccedcc06f640c0d1ba4c98528a297e1
Sergey Stadnik <sstadnik@odecee.com.au>
2018-04-12 19:03:14 +1000

    Added flow types

--- src/types.js
+++ src/types.js
@@ -2,10 +2,21 @@
 
 export type Role = {
   name: string,
-  action: string
+  actor: string
 };
 
 export type Movie = {
   name: string,
   roles: Array<Role>
+};
+
+export type ActorDataItem = {
+  actor: string,
+  role: string,
+  movie: string
+};
+
+export type ActorData = {
+  actor: string,
+  roles: Array<string>
 };
